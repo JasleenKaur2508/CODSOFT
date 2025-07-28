355f4162936cde2bbc8542ed0c164ee436575ebc
JasleenKaur2508 <hiyajasleen@gmail.com>
2025-07-28 21:04:57 +0530

    Add files via upload

---
+++ TASK-2 Calculator/script.js
@@ -0,0 +1,24 @@
+let display = document.getElementById("display");
+
+function append(value) {
+  if (display.value === "0" || display.value === "Error") {
+    display.value = value;
+  } else {
+    display.value += value;
+  }
+}
+
+function clearDisplay() {
+  display.value = "0";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
+
+
+
